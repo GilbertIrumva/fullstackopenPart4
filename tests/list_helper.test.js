0ccf133ab4e097280e7d6655c93a8c37deb20823
Gilbertirumva <gilbertirumva25@gmail.com>
2026-05-23 10:36:57 +0300

    completed the fourth exercise of part 4.4: Helper Functions and Unit Tests, step 2

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,11 +1,22 @@
-const { test } = require('node:test')
+const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
-test('dummy returns one', () => {
-  const blogs = []
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'my first unity test blog',
+      author: 'Gilbert',
+      url: 'http://www.example.com',
+      likes: 5,
+      __v: 0
+    }
+  ]
 
-  const result = listHelper.dummy(blogs)
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes([])
 
-  assert.strictEqual(result, 1)
+    assert.strictEqual(result, 0)
+  })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,16 @@
-const dummy = (logs) => {
+const dummy = (likes) => {
     return 1    
+}
+
+const totalLikes = (blogs) => {
+  const reducer = (sum, blog) => {
+    return sum + blog.likes
+  }
+
+  return blogs.reduce(reducer, 0)
 }
 
 module.exports = {
     dummy,  
+    totalLikes,
 }
