919dddedccde0706d963a8efc15d3f2138902b9c
Gilbertirumva <gilbertirumva25@gmail.com>
2026-05-25 17:34:42 +0300

    completion of exercise 4.11*: blog list tests, step 4

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -71,6 +71,22 @@ test('a valid blog can be added', async () => {
   assert.ok(titles.includes(newBlog.title));
 });
 
+test('if likes is missing, it defaults to 0', async () => {
+  const newBlog = {
+    title: 'No likes blog',
+    author: 'No Likes',
+    url: 'https://example.com/nolikes'
+  };
+
+  const response = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+
+  assert.strictEqual(response.body.likes, 0);
+});
+
 after(async () => {
   await mongoose.connection.close()
 })
